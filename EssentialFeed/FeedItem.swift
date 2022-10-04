453a02c022fc2ebb35248a6662418c7191823d04
Radoslaw Serek <radoslaw.serek@treelineinteractive.com>
2022-10-04 12:13:32 +0200

    Create FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Radosław Serek on 04/10/2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
