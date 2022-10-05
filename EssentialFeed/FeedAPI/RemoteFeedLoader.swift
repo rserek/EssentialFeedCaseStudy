8b9800651a3bb5f47d80ff5c242d8e39122145b1
Radoslaw Serek <radoslaw.serek@treelineinteractive.com>
2022-10-05 22:44:14 +0200

    Replace magic number with property giving it more context

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -71,8 +71,10 @@ private class FeedItemsMapper {
         }
     }
 
+    static var acceptableResponseCode = 200
+    
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else { throw RemoteFeedLoader.Error.invalidData }
+        guard response.statusCode == acceptableResponseCode else { throw RemoteFeedLoader.Error.invalidData }
         
         let root = try JSONDecoder().decode(Root.self, from: data)
         return root.items.map(\.feedItem)
