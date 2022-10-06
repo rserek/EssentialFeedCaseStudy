6d95e56bd2841edd9367524231d22fd140a082d1
Radoslaw Serek <radoslaw.serek@treelineinteractive.com>
2022-10-06 09:11:43 +0200

    Move FeedItemsMapper to separate file

---
+++ EssentialFeed/FeedAPI/FeedItemsMapper.swift
@@ -0,0 +1,34 @@
+//
+//  FeedItemsMapper.swift
+//  EssentialFeed
+//
+//  Created by Radosław Serek on 06/10/2022.
+//
+
+import Foundation
+
+internal class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var feedItem: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+
+    private static var acceptableResponseCode = 200
+    
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == acceptableResponseCode else { throw RemoteFeedLoader.Error.invalidData }
+        
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map(\.feedItem)
+    }
+}
