c703dd880a4363fb2cf4b94c2957a41e5904e2f9
Radoslaw Serek <radoslaw.serek@treelineinteractive.com>
2022-10-07 11:55:52 +0200

    Move `URLSessionHTTPClient` to its own file in production

---
+++ EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
@@ -0,0 +1,31 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Radosław Serek on 07/10/2022.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    private struct UnexpectedValuesRepresentation: Error { }
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
+
--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -5,30 +5,9 @@
 //  Created by Radosław Serek on 06/10/2022.
 //
 
+import EssentialFeed
 import Foundation
 import XCTest
-import EssentialFeed
-
-class URLSessionHTTPClient: HTTPClient {
-    let session: URLSession
-    
-    private struct UnexpectedValuesRepresentation: Error { }
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url, completionHandler: { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }).resume()
-    }
-}
 
 class URLSessionHTTPClientTests: XCTestCase {
     override func setUp() {
