f17541e0e2020d1e89bc01b6a7b45488adad69ad
Radoslaw Serek <radoslaw.serek@treelineinteractive.com>
2022-10-07 10:44:31 +0200

    Move creating URL to a factory method

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -39,7 +39,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_performsGETRequestWithURL() {
-        let url = URL(string: "https://a-url.com")!
+        let url = anyURL()
         let expectation = XCTestExpectation(description: "Wait for request")
         
         URLProtocolStub.observeRequests { request in
@@ -55,13 +55,12 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "https://a-url.com")!
         let error = NSError(domain: "Invalid request", code: 400)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
                 
         let expectation = XCTestExpectation(description: "Wait for completion")
         
-        makeSUT().get(from: url) { result in
+        makeSUT().get(from: anyURL()) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -82,6 +81,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
     
     private class URLProtocolStub: URLProtocol {
