6d9cb11d0a3222007249a474c447c63547c2b9f0
Radek Serek <radoslaw.serek@hotmail.com>
2023-01-27 14:03:36 +0100

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Radosław Serek on 27/01/2023.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 final public class FeedViewController: UITableViewController {
     private var loader: FeedLoader?
+    private var tableModel: [FeedImage] = []
     
     public convenience init(loader: FeedLoader) {
         self.init()
@@ -24,10 +25,27 @@ final public class FeedViewController: UITableViewController {
         load()
     }
     
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = cellModel.location == nil
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        
+        return cell
+    }
+    
     @objc private func load() {
         refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
+        loader?.load { [weak self] result in
             self?.refreshControl?.endRefreshing()
+            self?.tableModel = (try? result.get()) ?? []
+            self?.tableView.reloadData()
         }
     }
 }
