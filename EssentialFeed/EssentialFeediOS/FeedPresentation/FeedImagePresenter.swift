84581ef7e72241691d84f2f3e4168c7ccdfe762b
Radek Serek <radoslaw.serek@hotmail.com>
2023-02-05 19:45:11 +0100

    Move `FeedViewModel` to its own file

---
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedImagePresenter.swift
@@ -0,0 +1,67 @@
+//
+//  FeedImagePresenter.swift
+//  EssentialFeediOS
+//
+//  Created by Radosław Serek on 05/02/2023.
+//
+
+import EssentialFeed
+import Foundation
+
+final class FeedImagePresenter<Image> {
+    typealias Observer<T> = (T) -> Void
+    
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    private let imageTransformator: (Data) -> Image?
+    
+    private var task: FeedImageDataLoaderTask?
+
+    init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTransformator: @escaping (Data) -> Image?) {
+        self.model = model
+        self.imageLoader = imageLoader
+        self.imageTransformator = imageTransformator
+    }
+    
+    var location: String? {
+        return model.location
+    }
+    
+    var isLocationHidden: Bool {
+        return location == nil
+    }
+
+    var description: String? {
+        return model.description
+    }
+    
+    var onImageLoad: Observer<Image?>?
+    var onImageLoadingStateChange: Observer<Bool>?
+    var onAllowRetryingStateChange: Observer<Bool>?
+    
+    func loadFeedImage() {
+        onImageLoadingStateChange?(true)
+        onImageLoad?(nil)
+        onAllowRetryingStateChange?(false)
+        
+        task = self.imageLoader.loadImageData(from: model.url) { [weak self] result in
+            self?.handle(result)
+        }
+    }
+    
+    private func handle(_ result: FeedImageDataLoader.Result) {
+        let data = try? result.get()
+        let image = data.flatMap(imageTransformator) ?? nil
+        onImageLoad?(image)
+        onAllowRetryingStateChange?(image == nil)
+        onImageLoadingStateChange?(false)
+    }
+    
+    func preload() {
+        task = imageLoader.loadImageData(from: model.url) { _ in }
+    }
+    
+    func cancelLoad() {
+        task?.cancel()
+    }
+}
--- EssentialFeed/EssentialFeediOS/FeedPresentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedPresenter.swift
@@ -7,22 +7,6 @@
 
 import EssentialFeed
 
-struct FeedLoadingViewModel {
-    let isLoading: Bool
-}
-
-protocol FeedLoadingView {
-    func display(_ viewModel: FeedLoadingViewModel)
-}
-
-struct FeedViewModel {
-    let feed: [FeedImage]
-}
-
-protocol FeedView {
-    func display(_ viewModel: FeedViewModel)
-}
-
 final class FeedPresenter {
     private let loadingView: FeedLoadingView
     private let feedView: FeedView
