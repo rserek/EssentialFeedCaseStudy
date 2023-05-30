215e91662a2348c2c97b70e76bd7c5feb7ff0c40
Radek Serek <radoslaw.serek@hotmail.com>
2023-05-30 13:18:59 +0200

    Move SUT creation to test factory method

--- EssentialFeed/EssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
@@ -15,14 +15,21 @@ final class FeedImagePresenter {
 
 final class FeedImagePresenterTests: XCTestCase {
     func test_init_doesNotSendMessageToView() {
-        let view = ViewSpy()
-        
-        _ = FeedImagePresenter(view: view)
+        let (_, view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty)
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt64 = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        trackForMemoryLeaks(view)
+        trackForMemoryLeaks(sut)
+        
+        return (sut, view)
+    }
     
     private class ViewSpy {
         let messages = [Any]()
